3f6c94944d5b986d3087d1a39e3a8db2e26a7e72
abil samedov <abil.samedov2016@yandex.ru>
2023-03-05 20:54:10 +0400

    Testing, Cashing and Celery

---
+++ users/adapter.py
@@ -0,0 +1,11 @@
+# from allauth.account.adapter import DefaultAccountAdapter
+
+
+# class CustomSocialAccountAdapter(DefaultAccountAdapter):
+
+#     def save_user(self, request, sociallogin, form=None):
+#         user = super(CustomSocialAccountAdapter, self).save_user(request, sociallogin, form=form)
+#         return user
+
+#     def get_connect_redirect_url(self, request, socialaccount):
+#         return super(CustomSocialAccountAdapter, self).get_connect_redirect_url(request, socialaccount)
